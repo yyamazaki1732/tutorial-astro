516182f80a3398581536293af1a7caef4b5404e9
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-26 15:10:54 +0900

    fix: delete unrelated

--- src/content/blog/config.ts
+++ src/content/blog/config.ts
@@ -1,13 +1,13 @@
-import { defineCollection, z } from "astro:content";
+// import { defineCollection, z } from "astro:content";
 
-const blogCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    author: z.string(),
-    date: z.date(),
-  }),
-});
+// const blogCollection = defineCollection({
+//   schema: z.object({
+//     title: z.string(),
+//     author: z.string(),
+//     date: z.date(),
+//   }),
+// });
 
-export const collections = {
-  blog: blogCollection,
-};
+// export const collections = {
+//   blog: blogCollection,
+// };
