4584306c33e696089e4bc9386334a70c5f50b488
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-26 16:24:47 +0900

    update: add: display of a setting of language

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,15 +8,18 @@ export const defaultLang = "en";
 
 export const ui = {
   en: {
+    "lang.top": "en",
     "title.top": "Home Page",
     "heading.top": "My awesome blog subtitle",
     "common.top": "common area",
   },
   ja: {
+    "lang.top": "ja",
     "title.top": "ホームページ",
     "heading.top": "私のブログ",
   },
   fr: {
+    "lang.top": "fr",
     "title.top": "hōmupēji",
     "heading.top": "Mon blog génial sous-titre",
   },
