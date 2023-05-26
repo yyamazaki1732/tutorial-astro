d54ef9150020e28eaee75ea626df1ee5d6637c0e
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-26 14:42:21 +0900

    update: add multilingual support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+  en: "English",
+  ja: "日本語",
+  fr: "Français",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "nav.twitter": "Twitter",
+  },
+  ja: {
+    "nav.home": "富士山",
+    "nav.about": "À propos",
+  },
+  fr: {
+    "nav.home": "Acceuil",
+    "nav.about": "À propos",
+  },
+} as const;
