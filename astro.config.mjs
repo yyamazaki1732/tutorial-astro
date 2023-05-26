2ce7a715755517bf719a5b3de66476e81ea25d0a
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-05-26 17:08:15 +0900

    upgrade: set a environment variable

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: process.env.NODE_ENV === "internal" ? "/test" : "",
+  compressHTML: true,
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "entry.js",
+          chunkFileNames: "scripts/script.[hash].js",
+          assetFileNames: "styles/style.[hash][extname]",
+        },
+      },
+    },
+  },
+});
