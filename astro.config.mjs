a73de8c85959403e7b2962bf7f9834a8fc90d237
YAMAZAKI <130333162+yyamazaki0410@users.noreply.github.com>
2023-09-04 09:23:07 +0900

    add: conditional branch to settings: base in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,12 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  base: process.env.NODE_ENV === "internal" ? "/test" : "",
+  base:
+    process.env.NODE_ENV === "development"
+      ? "/"
+      : process.env.NODE_ENV === "production"
+      ? "/test/"
+      : null,
   compressHTML: true,
   vite: {
     build: {
