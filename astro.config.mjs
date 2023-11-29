fbc4eb39be3573063dd81eb4b64bb8235dc6cf68
YAMAZAKI <yusuke.yamazaki98@gmail.com>
2023-11-29 15:18:49 +0900

    add: tialwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,31 @@
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === "development"
-      ? "/"
-      : process.env.NODE_ENV === "production"
-      ? "/test/"
-      : null,
+  base: process.env.NODE_ENV === "development" ? "/" : process.env.NODE_ENV === "production" ? "/test/" : null,
   compressHTML: false,
   outDir: "./my-custom-build-directory",
-  server: { port: 1234, host: true },
+  server: {
+    port: 1234,
+    host: true
+  },
   vite: {
     build: {
       buildOptions: {
         env: {
-          NODE_ENV: process.env.NODE_ENV,
-        },
+          NODE_ENV: process.env.NODE_ENV
+        }
       },
       rollupOptions: {
         output: {
           entryFileNames: "entry.js",
           chunkFileNames: "scripts/script.[hash].js",
-          assetFileNames: "styles/style.[hash][extname]",
-        },
-      },
-    },
+          assetFileNames: "styles/style.[hash][extname]"
+        }
+      }
+    }
   },
+  integrations: [tailwind()]
 });
