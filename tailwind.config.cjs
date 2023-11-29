e01c9df5f319b7ef05e497da101da17691f899bd
YAMAZAKI <yusuke.yamazaki98@gmail.com>
2023-11-29 17:01:03 +0900

    wip: adjust style & directory structure for figma token test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+import { colors, spacing } from "./data/tokentest.json";
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.red.value,
+      },
+      spacing: {
+        4: `${spacing[4].value}px`,
+        8: `${spacing[8].value}px`,
+        12: `${spacing[12].value}px`,
+      },
+    },
+  },
+  plugins: [],
+};
