c65138d49f86caed343e6faaff0ad655e08eada9
YAMAZAKI <yusuke.yamazaki98@gmail.com>
2023-11-30 18:45:11 +0900

    make it enable to use tokens.json as taiwind class

--- plugins/editValue.js
+++ plugins/editValue.js
@@ -1,3 +1,25 @@
-import { global } from "../data/tokens.json";
+const fs = require("fs");
 
-export const Style = global;
+const data = JSON.parse(fs.readFileSync("./data/tokens.json", "utf8"));
+
+function replaceColorValues(obj, globalColors) {
+  Object.keys(obj).forEach((key) => {
+    if (typeof obj[key] === "object") {
+      replaceColorValues(obj[key], globalColors); // 再帰的に探索
+    } else if (
+      typeof obj[key] === "string" &&
+      obj[key].startsWith("{") &&
+      obj[key].endsWith("}")
+    ) {
+      let colorValue = globalColors;
+      let colorRef = obj[key].slice(1, -1).split("."); // "colors.gray.900" ->
+      let colorObj = colorValue[colorRef[1]][colorRef[2]];
+
+      obj[key] = typeof colorObj === "object" ? colorObj.value : colorObj; // 参照の置き換え
+    }
+  });
+}
+
+replaceColorValues(data.theme.colors, data.global.colors);
+// console.log(JSON.stringify(data, null, 2));
+module.exports = data;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-const Style = require("./plugins/editValue.js").Style;
+const data = require("./plugins/editValue.js");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        primary: Style.colors.black.value,
-      },
+      colors: () => ({
+        text: data.theme.colors.text,
+        surface: data.theme.colors.surface,
+      }),
     },
   },
   plugins: [],
