817de3fe13b7f30235d3ee7f55db28e61e71565a
YAMAZAKI <yusuke.yamazaki98@gmail.com>
2023-11-30 10:55:18 +0900

    fix: import proper module from tokens.json

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-import { colors, spacing } from "./data/tokentest.json";
+import { core } from "./data/tokens.json";
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
-        primary: colors.red.value,
-      },
-      spacing: {
-        4: `${spacing[4].value}px`,
-        8: `${spacing[8].value}px`,
-        12: `${spacing[12].value}px`,
+        primary: core.colors.black.value,
       },
     },
   },
